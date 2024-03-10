b14802d580e91cf219e3c43632f38e057d8aa822
alamin6688 <alamin.kzs06@gmail.com>
2024-03-10 13:37:03 +0600

    core event,useState, useEffect

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css'
+import Counter from './Counter'
+import Friends from './Friends'
+import Team from './Team'
+import User from './Users'
+
+function App() {
+
+  function handleClick(){
+    alert('button clicked')
+  }
+
+  const handleClick2 = () =>{
+    alert('button 2 clicked')
+  }
+
+  const addToFive = (num) =>{
+    alert(num + 5);
+  }
+
+  return (
+    <>
+      <h3>React Core Concepts 2</h3>
+
+    <Friends></Friends>
+      
+    <User></User>
+
+    <Team></Team>
+
+    <Counter></Counter>
+
+    <button onClick={handleClick}>Click Me</button>
+    <button onClick={handleClick2}>Click2</button>
+    <button onClick={()=>{alert('third clicked')}}>third</button>
+    {/* Vejailla */}
+    <button onClick={()=> addToFive(3)}>Four</button>
+    </>
+  )
+}
+
+export default App
